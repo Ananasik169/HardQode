ce3d78c678faf7f38d13d65ff1b04ec5f77a205e
Pon4ik <borzikovu32@gmail.com>
2023-09-22 22:16:38 +0300

    Напсиал API для всех уроков доступных пользователю и API для всех уроков 1 из продуктов, доступных пользователю.

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,12 +1,28 @@
-from rest_framework.serializers import ModelSerializer
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.serializers import ModelSerializer, CharField, DurationField
 from products.models import Lesson, LessonProgress, Product, ProductAccess
 
 
-class LessonSerializer(ModelSerializer):
+class BaseLessonSerializer(ModelSerializer):
+    custom_fields = ['viewed_time', 'status']
 
     class Meta:
         model = Lesson
-        fields = "__all__"
+        fields = ["title", "video_url", "duration"]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        try:
+            lesson_progress = instance.lesson_progress.get(user=self.context['request'].user)
+        except ObjectDoesNotExist:
+            data.update({field: None for field in self.custom_fields})
+        else:
+            data.update({field: getattr(lesson_progress, field) for field in self.custom_fields})
+        return data
+    
+
+class RetriveLessonSerializer(BaseLessonSerializer):
+    custom_fields = custom_fields = ['viewed_time', 'status', 'last_view']
 
 
 class ProductSerializer(ModelSerializer):
--- src/api/views.py
+++ src/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from products.models import Lesson, ProductAccess, Product, LessonProgress
-from api.serializers import LessonSerializer
+from api.serializers import BaseLessonSerializer, RetriveLessonSerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -10,16 +10,17 @@ class LessonViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = Lesson.objects.filter(
             product_set__in=Product.objects.filter(access__user=request.user)
-        )
-        serailizer = LessonSerializer(queryset, many=True)
+        ).distinct()
+        serailizer = BaseLessonSerializer(queryset, many=True, context={'request': request})
         return Response(serailizer.data, status=status.HTTP_200_OK)
 
-    def retrieve(self, request, product_id):
-        product = get_object_or_404(Product, id=product_id)
-        if ProductAccess.objects.filter(product=product, user=request.user).exists():
-            serailizer = LessonSerializer(product.lessons, many=True)
+    def retrieve(self, request, pk=None):
+        product = get_object_or_404(Product, id=pk)
+        if ProductAccess.objects.filter(product=product, user=request.user):
+            serailizer = RetriveLessonSerializer(product.lessons, many=True, context={'request': request})
             return Response(serailizer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_403_FORBIDDEN)
+
 
 class ProductViewSet(viewsets.ViewSet):
     
